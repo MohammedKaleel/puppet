1a3bd6741103f7df9980ae3e8dd44f8d61947b3a
changed by puppet <root@learning.puppetlabs.vm>
2019-04-07 16:20:20 -0700

    pass removed

--- manifests/user.pp
+++ manifests/user.pp
@@ -3,7 +3,6 @@ user { 'root':
   comment            => 'changed by puppet',
   gid                => 0,
   home               => '/root',
-  password           => '$1$JbAp9Kio$i1Xk1gyduE08PFWnArczT.',
   password_max_age   => 99999,
   password_min_age   => 0,
   password_warn_days => 7,
